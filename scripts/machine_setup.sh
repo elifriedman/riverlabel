bf38d3d28f73561e5756421a8c67e569235d7cd0
Eli Friedman <eli@friedman>
2026-08-27 23:32:36 +0300

    prompt to install

--- scripts/machine_setup.sh
+++ scripts/machine_setup.sh
@@ -9,9 +9,16 @@ venv/bin/pip install git+https://github.com/elifriedman/label-studio.git@timeser
 venv/bin/python venv/lib/python3.11/site-packages/label_studio/manage.py collectstatic
 venv/bin/pip install -r requirements.txt
 
-# optional: if you want to run the label studio and the admin pages
-# even when you're not logged in, then run the following:
-sudo install -m 0644 services/label_studio.service /etc/systemd/system/label_studio.service
-sudo install -m 0644 services/admin_api.service /etc/systemd/system/admin_api.service
-sudo systemctl daemon-reload
-sudo systemctl enable --now label_studio.service admin_api.service
+install_services="n"
+if [[ -t 0 ]]; then
+    read -r -p "Should Label Studio and the RiverLabel admin website start automatically in the background, including after a restart? This is for if you're running on a server. Respond with y or n " install_services || true
+fi
+
+if [[ "$install_services" =~ ^[Yy]$ ]]; then
+    sudo install -m 0644 services/label_studio.service /etc/systemd/system/label_studio.service
+    sudo install -m 0644 services/admin_api.service /etc/systemd/system/admin_api.service
+    sudo systemctl daemon-reload
+    sudo systemctl enable --now label_studio.service admin_api.service
+else
+    echo "Skipping systemd service installation."
+fi
